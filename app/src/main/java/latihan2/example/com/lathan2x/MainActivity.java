5fd5ce46f334626c74e6602f1f4cfd5fda971c27
widhy25 <widiikhoerunnisa25@gmail.com>
2020-08-13 12:28:41 +0700

    1

---
+++ app/src/main/java/latihan2/example/com/lathan2x/MainActivity.java
@@ -0,0 +1,34 @@
+package latihan2.example.com.lathan2x;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtNama;
+    Button btnKlik;
+    TextView txtHasil;
+    String nama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtNama=(EditText)findViewById(R.id.edt_nama);
+        btnKlik=(Button)findViewById(R.id.btn_klik);
+        txtHasil=(TextView)findViewById(R.id.txt_hasil);
+
+        btnKlik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama=edtNama.getText().toString();
+                txtHasil.setText(nama);
+
+            }
+        });
+    }
+}
